1d7b426634ce8cbb28e6e3918fc3850238bd6c89
Laura Lemmo <laura.lemmo@stud.unifi.it>
2024-10-10 11:48:49 +0200

    class added

---
+++ NormalABR.py
@@ -0,0 +1,45 @@
+from Node import Node
+
+
+class NormalABR:
+
+    def __init__(self):
+        self.root = None
+
+    def setRoot(self, key):
+        self.root = Node(key)
+
+
+
+    def insert(self, key):
+        if (self.root is None):
+           self.setRoot(key)
+        else:
+            self.insertNode(self.root, key)
+
+    def insertNode(self, currentNode, key):
+        if (key <= currentNode.key):
+            if (currentNode.left):
+                self.insertNode(currentNode.left, key)
+            else:
+                currentNode.left = Node(key)
+        elif (key > currentNode.key):
+            if (currentNode.right):
+                self.insertNode(currentNode.right, key)
+            else:
+                currentNode.right = Node(key)
+
+
+
+    def find(self, key):
+        return self.findNode(self.root, key)
+
+    def findNode(self, currentNode, key):
+        if (currentNode is None):
+            return False
+        elif (key == currentNode.key):
+            return True
+        elif (key < currentNode.key):
+            return self.findNode(currentNode.left, key)
+        else:
+            return self.findNode(currentNode.right, key)
