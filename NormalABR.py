571a816459c1929feaf8dabbe619e2ae93a759ac
Laura Lemmo <laura.lemmo@stud.unifi.it>
2024-10-11 15:39:34 +0200

    update

--- NormalABR.py
+++ NormalABR.py
@@ -43,3 +43,55 @@ class NormalABR:
             return self.findNode(currentNode.left, key)
         else:
             return self.findNode(currentNode.right, key)
+
+
+
+
+    def delete(self, key):
+        self.search(None, self.root, key)
+
+
+    def search(self, pNode, currentNode, key):
+        if (currentNode is None):
+            return False
+        elif (currentNode.key == key):
+            self.deleteNode(pNode, currentNode)
+        elif (key < currentNode.key):
+            pNode = currentNode
+            self.search(pNode, currentNode.left, key)
+        else:
+            pNode = currentNode
+            self.search(pNode, currentNode.right, key)
+
+
+    def deleteNode(self, pNode, removableNode):
+        if (removableNode.left == None):
+            self.modify(pNode, removableNode, removableNode.right)
+        elif (removableNode.right == None):
+            self.modify(pNode, removableNode, removableNode.left)
+        else:
+            minimumNode = self.minimum(removableNode.right)   #parametri
+            if (minimumNode):                                 #condizione if
+                self.modify(..., minimumNode, minimumNode.right)   #mi serve il padre
+                minimumNode.right = removableNode.right
+            self.modify(pNode, removableNode, minimumNode)
+            minimumNode.left = removableNode.left
+
+
+
+
+    def modify(self, pNode, removableNode, newNode):
+        if (pNode is None):
+            self.root = newNode
+        elif (removableNode == pNode.left):
+            pNode.left = newNode
+        else:
+            pNode.right = newNode
+
+
+    def minimum(self, newNode):            #devo fornire anche il padre
+        if (newNode.left != None):
+            self.minimum(newNode.left)
+        return newNode
+
+
