97cf6f17b616371faeba74d062f70d1a662a47d3
Laura Lemmo <laura.lemmo@stud.unifi.it>
2024-10-13 12:40:36 +0200

    update

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from NormalABR import NormalABR
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+normalABR = NormalABR()
+normalABR.insert(7)
+normalABR.insert(1)
+normalABR.insert(4)
+normalABR.insert(10)
+normalABR.insert(8)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+normalABR.inorder()
