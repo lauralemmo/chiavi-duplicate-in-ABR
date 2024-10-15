250c15578cc6504bd201149530fb2dfb6109a954
Laura Lemmo <laura.lemmo@stud.unifi.it>
2024-10-15 12:44:43 +0200

    update

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from NormalABR import NormalABR
 
 normalABR = NormalABR()
+
 normalABR.insert(7)
 normalABR.insert(1)
 normalABR.insert(4)
 normalABR.insert(10)
 normalABR.insert(8)
+
+normalABR.inorder()
+print("\n")
+
+normalABR.delete(4)
 
 normalABR.inorder()
