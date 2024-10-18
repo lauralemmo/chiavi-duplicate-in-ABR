b81e2c2eb0e3d70943b8f416d4cef4eabefbcd52
Laura Lemmo <laura.lemmo@stud.unifi.it>
2024-10-18 15:25:35 +0200

    update

--- BooleanABR.py
+++ BooleanABR.py
@@ -30,15 +30,43 @@ class BooleanABR:
         else:
             self.insertNode(self.root, key)
 
+    #def insertNode(self, currentNode, key):
+    #    if (key == currentNode.key):
+    #        if (currentNode.flag):
+    #            n = currentNode.left
+    #            currentNode.left = Node(key)
+    #            currentNode.left.left = n
+    #            currentNode.flag = False
+    #        else:
+    #            n = currentNode.right
+    #            currentNode.right = Node(key)
+    #            currentNode.right.right = n
+    #            currentNode.flag = True
+    #    elif (key < currentNode.key):
+    #        if (currentNode.left):
+    #            self.insertNode(currentNode.left, key)
+    #        else:
+    #            currentNode.left = Node(key)
+    #    elif (key > currentNode.key):
+    #        if (currentNode.right):
+    #            self.insertNode(currentNode.right, key)
+    #        else:
+    #            currentNode.right = Node(key)
+
     def insertNode(self, currentNode, key):
         if (key == currentNode.key):
             if (currentNode.flag):
-                currentNode.left = Node(key)
                 currentNode.flag = False
+                if (currentNode.left):
+                    self.insertNode(currentNode.left, key)
+                else:
+                    currentNode.left = Node(key)
             else:
-                currentNode.right = Node(key)
                 currentNode.flag = True
-
+                if (currentNode.right):
+                    self.insertNode(currentNode.right, key)
+                else:
+                    currentNode.right = Node(key)
         elif (key < currentNode.key):
             if (currentNode.left):
                 self.insertNode(currentNode.left, key)
@@ -49,3 +77,78 @@ class BooleanABR:
                 self.insertNode(currentNode.right, key)
             else:
                 currentNode.right = Node(key)
+
+
+    def find(self, key):
+        return self.findNode(self.root, key)
+
+    def findNode(self, currentNode, key):
+        if (currentNode is None):
+            return False
+        elif (key == currentNode.key):
+            return True
+        elif (key < currentNode.key):
+            return self.findNode(currentNode.left, key)
+        else:
+            return self.findNode(currentNode.right, key)
+
+
+
+
+    def delete(self, key):
+        self.search(None, self.root, key)
+
+
+    def search(self, pNode, currentNode, key):
+        if (currentNode is None):
+            return False
+        elif (currentNode.key == key):
+            self.deleteNode(pNode, currentNode)
+        elif (key < currentNode.key):
+            pNode = currentNode
+            self.search(pNode, currentNode.left, key)
+        else:
+            pNode = currentNode
+            self.search(pNode, currentNode.right, key)
+
+
+    def deleteNode(self, pNode, removableNode):
+        if (removableNode.left == None):
+            self.modify(pNode, removableNode, removableNode.right)
+        elif (removableNode.right == None):
+            self.modify(pNode, removableNode, removableNode.left)
+        else:
+            minimumNode, pMinimumNode = self.minimum(removableNode, removableNode.right)
+            if (pMinimumNode != removableNode):
+                self.modify(pMinimumNode, minimumNode, minimumNode.right)
+                minimumNode.right = removableNode.right
+            self.modify(pNode, removableNode, minimumNode)
+            minimumNode.left = removableNode.left
+
+
+    def modify(self, pNode, removableNode, newNode):
+        if (pNode is None):
+            self.root = newNode
+        elif (removableNode == pNode.left):
+            pNode.left = newNode
+        else:
+            pNode.right = newNode
+
+
+    def minimum(self, pNode, newNode):
+        if (newNode.left != None):
+            pNode = newNode
+            self.minimum(pNode, newNode.left)
+        return newNode, pNode
+
+
+
+    def inorder(self):
+        self.inorderTree(self.root)
+
+
+    def inorderTree(self, node):
+        if (node != None):
+            self.inorderTree(node.left)
+            print(node.key)
+            self.inorderTree(node.right)
