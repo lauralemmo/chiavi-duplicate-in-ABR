c36e6ce01468f39b7bb574b3e2cdb9c76f0efe2d
Laura Lemmo <laura.lemmo@stud.unifi.it>
2024-10-20 16:57:46 +0200

    update for insertion

--- ListABR.py
+++ ListABR.py
@@ -11,12 +11,17 @@
 
 
 
+
+class Node:
+    def __init__(self):
+        self.next = None
+
+
 class LinkedListNode:
     def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
-        self.next = None
         self.head = None
 
 
@@ -29,8 +34,9 @@ class ListABR:
 
 
     def setRoot(self, key):
-        self.root = LinkedListNode(key)
-        LinkedListNode(key).head = self.root
+        a = LinkedListNode(key)
+        self.root = a
+        a.head = Node()                               #METTI N AL POSTO DI A PER NODI E L PER LISTE
 
     def insert(self, key):
         if (self.root is None):
@@ -38,23 +44,22 @@ class ListABR:
         else:
             self.insertNode(self.root, key)
 
-    def insertNode(self, currentNode, key):
-        if (key == currentNode.key):
-            currentNode.next = Node(key)
-            currentNode.
-        elif (key < currentNode.key):
-            if (currentNode.left):
-                self.insertNode(currentNode.left, key)
+    def insertNode(self, currentList, key):
+        if (key == currentList.key):
+            a = Node()
+            a.next = currentList.head     #aggiungo in testa
+            currentList.head = a
+        elif (key < currentList.key):
+            if (currentList.left):
+                self.insertNode(currentList.left, key)
             else:
-                currentNode.left = Node(key)
-        elif (key > currentNode.key):
-            if (currentNode.right):
-                self.insertNode(currentNode.right, key)
+                a = LinkedListNode(key)
+                currentList.left = a
+                a.head = Node()
+        elif (key > currentList.key):
+            if (currentList.right):
+                self.insertNode(currentList.right, key)
             else:
-                currentNode.right = Node(key)
-
-
-
-    #SISTEMA NODI CHE HANNO SET E GET SOLO NELLA CLASSE NODE
-    #MENTRE QUELLE MESSE NELLE ALTRE CLASSI NON HANNO METODI
-    #PROVA A TOGLIERE IMPORT E GUARDA SE FUNZIONA LO STESSO
+                a = LinkedListNode(key)
+                currentList.right = a
+                a.head = Node()
